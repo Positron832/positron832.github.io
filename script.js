d6223d9b8287aae7019f6585c98f2fb53214cc1c
Positron832 <72881516+Positron832@users.noreply.github.com>
2021-03-07 15:35:08 +0100

    Update script.js

--- script.js
+++ script.js
@@ -4,3 +4,6 @@ text.onmouseout = function(){unHov()};
 function hov() {
  text.innerHTML = "Test.";
 }
+function unhov() {
+ text.innerHTML = "This site is used to host files that are used in <a href="https:\/\/positron832.neocities.org">my Neocities website</a> and for proper XHTML testing.";
+}
