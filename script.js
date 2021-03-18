6a14dd60d64a94415c0e5bcfc1e0f381e21727a5
Positron832 <72881516+Positron832@users.noreply.github.com>
2021-03-18 16:49:52 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,8 +1,9 @@
-let text = document.querySelector("span");
-text.onmouseover = function(){hov()};
-text.onmouseout = function(){unHov()};
+"use strict";
+let text = document.getElementById("text");
+text.onmouseover = function(){hov();}
+text.onmouseout = function(){unHov();}
 function hov() {
- text.innerHTML = "haaa haaa";
+ text.textContent = "HA HA!";
 }
 function unHov() {
  text.innerHTML = "This site is used to host files that are used in <a href='https:\/\/positron832.neocities.org'>my Neocities website</a> and for proper XHTML testing.";
